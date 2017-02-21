175c6ed1e54b0df48a316b3a91158d0511d4418d
Shannon Dowley <shannon.dowley@gmail.com>
2017-02-21 20:18:48 +1000

    Updating text handler

--- src/main/webapp/js/ds.js
+++ src/main/webapp/js/ds.js
@@ -3,15 +3,13 @@ var game = null;
 var timeInterval = null;
 var outageTime = null;
 
-dwr.engine.setTextHtmlHandler(function () {
-    window.alert("Your session has expired, please login again.");
-    document.location = '/jol/';
-});
-
 function errorhandler(errorString, exception) {
-    if (exception.name == "dwr.engine.incompleteReply") {
+    if (exception.name === "dwr.engine.incompleteReply") {
         window.alert("Lost connection with the server, reloading..");
         document.location = "/jol/";
+    } else if (exception.name === "dwr.engine.missingData") {
+        window.alert("Your session has expired, please login again.");
+        document.location = '/jol/';
     }
 }
 
