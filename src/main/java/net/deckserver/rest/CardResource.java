792ef7e283b63d390640827447a3b1def31c2754
Shannon Dowley <shannon.dowley@gmail.com>
2018-03-07 16:24:30 +1000

    Optimized imports

--- src/main/java/net/deckserver/rest/CardResource.java
+++ src/main/java/net/deckserver/rest/CardResource.java
@@ -7,8 +7,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.*;
-import java.util.Date;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
