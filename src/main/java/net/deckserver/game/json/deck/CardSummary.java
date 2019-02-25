2d82687d344898073c9e6dc040df019d74402a83
Shannon Dowley <shannon.dowley@gmail.com>
2019-02-26 00:07:39 +1000

    Updates to card api service

--- src/main/java/net/deckserver/game/json/deck/CardSummary.java
+++ src/main/java/net/deckserver/game/json/deck/CardSummary.java
@@ -21,4 +21,5 @@ public class CardSummary {
     private boolean crypt;
     private boolean unique;
     private String group;
+    private String sect;
 }
--- src/main/java/net/deckserver/rest/ApiResource.java
+++ src/main/java/net/deckserver/rest/ApiResource.java
@@ -6,27 +6,46 @@ import net.deckserver.game.json.deck.CardSummary;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Path("/api")
 public class ApiResource {
 
-    @GET
-    @Path("/cards")
-    @Produces("application/json")
-    public List<CardSummary> cardSummaries() {
+    private Map<String, CardSummary> cardSummaryMap = new HashMap<>();
+
+    public ApiResource() {
         File cardFile = Paths.get(System.getenv("JOL_DATA"), "cards", "cards.json").toFile();
         ObjectMapper objectMapper = new ObjectMapper();
         CollectionType cardSummaryCollectionType = objectMapper.getTypeFactory().constructCollectionType(List.class, CardSummary.class);
         try {
-            return objectMapper.readValue(cardFile, cardSummaryCollectionType);
+            List<CardSummary> summaries = objectMapper.readValue(cardFile, cardSummaryCollectionType);
+            cardSummaryMap = summaries.stream().collect(Collectors.toMap(CardSummary::getJolId, Function.identity()));
         } catch (IOException e) {
-            return new ArrayList<>();
+            System.err.println("Unable to read card data : " + e.getMessage());
         }
+    }
+
+    @GET
+    @Path("/cards")
+    @Produces("application/json")
+    public Collection<CardSummary> cardSummaries() {
+        return cardSummaryMap.values();
+    }
+
+    @GET
+    @Path("/cards/{id}")
+    @Produces("application/json")
+    public CardSummary cardSummary(@PathParam("id") String id) {
+        return cardSummaryMap.get(id);
     }
 }
