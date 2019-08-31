6d32faa040b6422fa40d636e19c68e5ee663e384
Dan Peschman <dpeschman@gmail.com>
2019-08-30 22:30:10 -0700

    split card modal script to separate file

---
+++ src/main/webapp/js/card-modal.js
@@ -0,0 +1,287 @@
+"use strict";
+var CLAN_CHARS = {
+  abomination: 'A', arihmane: 'B', akunanse: 'C', assamite: 'D', baali: 'E',
+  blood_brothers: 'F', brujah: 'G', brujah_antitribu: 'H', caitiff: 'I',
+  daughters_of_cacophony: 'J', follower_of_set: 'K', gangrel: 'L',
+  gangrel_antitribu: 'M', gargoyle: 'N', giovanni: 'O', guruhi: 'P',
+  harbingers_of_skulls: 'Q', ishtarri: 'R', kiasyd: 'S', lasombra: 'T',
+  malkavian: 'U', malkavian_antitribu: 'V', nagaraja: 'W', nosferatu: 'X',
+  nosferatu_antitribu: 'Y', osebo: 'Z', pander: '[', ravnos: '\\',
+  salubri: ']', salubri_antitribu: '^', samedi: '_', toreador: '`',
+  toreador_antitribu: 'a', tremere: 'b', tremere_antitrubu: 'c',
+  true_brujah: 'd', tzimisce: 'e', ventrue: 'f', ventrue_antitribu: 'g',
+  avenger: 'h', defender: 'i', innocent: 'j', judge: 'k', martyr: 'l',
+  redeemer: 'm', visionary: 'n'
+};
+var DISCIPLINE_CHARS = {
+  ani: 'a', ANI: 'A', obe: 'b', OBE: 'B', cel: 'c', CEL: 'C',
+  dom: 'd', DOM: 'D', dem: 'e', DEM: 'E', for: 'f', FOR: 'F',
+  san: 'g', SAN: 'G', thn: 'h', THN: 'H', vic: 'i', VIC: 'I',
+  pro: 'j', PRO: 'J', chi: 'k', CHI: 'K', val: 'l', VAL: 'L',
+  mel: 'm', MEL: 'M', nec: 'n', NEC: 'N', obf: 'o', OBF: 'O',
+  pot: 'p', POT: 'P', qui: 'q', QUI: 'Q', pre: 'r', PRE: 'R',
+  ser: 's', SER: 'S', tha: 't', THA: 'T', aus: 'u', AUS: 'U',
+  vis: 'v', VIS: 'V', abo: 'w', ABO: 'W', myt: 'x', MYT: 'X',
+  dai: 'y', DAI: 'Y', spi: 'z', SPI: 'Z', obt: '*', OBT: '+',
+  tem: '(', TEM: ')', str: ':', STR: ',', mal: '<', MAL: '>',
+  flight: '='
+};
+function cardTypeCSSClass(cardType) {
+  return cardType.toLowerCase().replace(' ', '_').replace('/', ' ');
+}
+function clanToKey(clan) {
+  return clan.toLowerCase().replace(/ /g, '_');
+}
+function showCardModal(event) {
+  $('#cardModal .loaded').hide();
+  $('#cardModal .loading').show();
+  //Show modal immediately with Loading text
+  //$('#cardModal').modal('show');
+  var cardId = $(event.target).data('card-id');
+  var handCoord = $(event.target).data('coordinates');
+  $.get({
+      //url: "rest/api/cards/lo0", success: function(card) { //Abombwe
+      //url: "rest/api/cards/lo7", success: function(card) { //Akhunanse Kholo
+      //url: "rest/api/cards/km98", success: function(card) { //Pack Alpha
+      //url: "rest/api/cards/jy322", success: function(card) { //Raven Spy
+      //url: "rest/api/cards/tr14", success: function(card) { //CrimethInc
+      //url: "rest/api/cards/lo170", success: function(card) { //Reanimated Corpse - an ally
+      //url: "rest/api/cards/lo22", success: function(card) { //Base Hunting Ground - pool cost and clan requirement
+      //url: "rest/api/cards/una21", success: function(card) { //Ennoia's Theater - multiple clans
+      //url: "rest/api/cards/au36", success: function(card) { //Make the Misere - multiple lines in preamble
+      //url: "rest/api/cards/au32", success: function(card) { //Guardian Vigil - multi-mode
+      //url: "rest/api/cards/bl61", success: function(card) { //Elemental Stoicism - modes require multiple disciplines
+      url: "rest/api/cards/" + cardId, success: function(card) {
+        console.log(card);
+        var modal = $('#cardModal');
+        modal.data('hand-coord', handCoord);
+        modal.data('do-not-replace', card.doNotReplace);
+
+        $('#cardModal .card-name').text(card.displayName);
+        $('#cardModal .card-type')
+          .removeClass()
+          .addClass('card-type ' + cardTypeCSSClass(card.type));
+        $('#cardModal .preamble').text(card.preamble);
+
+        var clanText = '';
+        if (card.clans != null) {
+          for (var c of card.clans)
+            clanText += CLAN_CHARS[clanToKey(c)];
+        }
+        $('#cardModal .card-clan').text(clanText);
+
+        var costText = null;
+        if (card.cost != null) costText = 'Costs ' + card.cost + '.';
+        $('#cardModal .card-cost').text(costText);
+
+        var modeContainer = $('#cardModal .card-modes');
+        modeContainer.empty();
+
+        var minionsConfigured = false;
+        var allModesTargetSelf = true;
+
+        if (card.modes && card.modes.length > 0) {
+          var modeTemplate = $('#cardModal .templates .card-mode');
+          for (var i = 0; i < card.modes.length; ++i) {
+            var mode = card.modes[i];
+            var button = modeTemplate.clone();
+
+            var extendedPlayPanel = $('#cardModal .extended-play-panel');
+            if (card.multiMode) {
+              extendedPlayPanel.show();
+              var playButton = $('#cardModalPlayButton');
+              playButton.prop('disabled', true);
+              playButton.text('Select one or more disciplines');
+              button.on('click', multiModeButtonClicked);
+            }
+            else {
+              extendedPlayPanel.hide();
+              button.on('click', modeClicked);
+            }
+
+            button.data('disciplines', mode.disciplines);
+            button.data('target', mode.target);
+            var disciplineStr = '';
+            if (mode.disciplines != null) {
+              for (var d of mode.disciplines)
+                disciplineStr += DISCIPLINE_CHARS[d];
+            }
+            button.children('.discipline').text(disciplineStr);
+            button.children('.mode-text').text(mode.text);
+            button.appendTo(modeContainer);
+
+            //if (mode.target == 'SELF') {
+            //  if (!minionsConfigured) {
+            //    configureMinionButtons();
+            //    minionsConfigured = true;
+            //  }
+            //}
+            //else {
+            //  allModesTargetSelf = false;
+            //}
+          }
+          //$('#cardModal .who-is-playing-panel').toggle(allModesTargetSelf);
+        }
+        //if (!minionsConfigured) $('who-is-playing-panel').hide();
+        $('#cardModal .loading').hide();
+        $('#cardModal .loaded').show();
+        tippy.hideAll({ duration: 0 });
+        $('#cardModal').modal('show');
+      }
+  });
+}
+function configureMinionButtons() {
+  var minionContainer = $('#cardModal .card-minions');
+  minionContainer.empty();
+
+  var minionTemplate = $('#cardModal .templates .card-minion');
+  //TODO fetch from forthcoming minion API
+  var minions = [
+    {index: 1, name: 'Suzy', type: 'vampire', blood: 3, capacity: 7, locked: true},
+    {index: 3, name: 'Ambrosio Luis Monçada, Plenipotentiary', type: 'vampire', blood: 10, capacity: 10, locked: false},
+    {index: 10, name: 'Reanimated Corpse', type: 'ally', life: 2, locked: false, label: 'Sammy'}
+  ];
+  for (var i = 0; i < minions.length; ++i) {
+    var minion = minions[i];
+    var button = minionTemplate.clone();
+    button.data('region-index', minion.index);
+    button.children('.minion-name').text(minion.name);
+    var life = button.children('.minion-life');
+    switch (minion.type) {
+      case 'vampire':
+        life.text(minion.blood + ' / ' + minion.capacity);
+        life.removeClass('life').addClass('blood');
+        break;
+      case 'ally':
+        life.text(minion.life);
+        life.removeClass('blood').addClass('life');
+        break;
+      default:
+        console.log('Card Modal: unknown minion type: ' + minion.type);
+        break;
+    }
+    button.children('.minion-locked').toggle(minion.locked);
+    var label = button.children('.minion-label');
+    label.text(minion.label);
+    label.toggle(minion.label != null);
+    button.children('.minion-index').text('(#' + minion.index + ')');
+    button.on('click', minionClicked);
+    button.appendTo(minionContainer);
+  }
+}
+function minionClicked(event) {
+  var button = $(event.target).closest('button');
+  var minions = $('#cardModal .card-minions');
+  minions.children().removeClass('active');
+
+  var extendedPlayPanel = $('#cardModal .extended-play-panel');
+  var multiMode = extendedPlayPanel.css('display') != 'none';
+  var modesSelected = $('#cardModal .card-modes button.active');
+  if (!multiMode && modesSelected.length > 0) {
+    //Not needed for visual effects or state change.  Only needed in case the
+    //card play is happening now, before the state change takes effect normally.
+    button.addClass('active');
+    playCard({target: modesSelected.eq(0)});
+  }
+}
+function modeClicked(event) {
+  var button = $(event.target).closest('button');
+  var minionPanel = $('#cardModal .who-is-playing-panel');
+  //minionPanel.toggle(button.data('target') == 'SELF');
+
+  if (minionPanel.css('display') == 'none'
+      || $('#cardModal .card-minions button.active').length > 0) {
+    var target = button.data('target');
+    if (target == 'SELF' || target == 'SOMETHING')
+      showTargetPicker(target);
+    else playCard(event);
+  }
+  else {
+    $('#cardModal .card-modes button').removeClass('active');
+  }
+}
+function showTargetPicker(target) {
+  var picker = $('#targetPicker');
+  $('#targetPicker .card-name').text($('#cardModalLabel').text());
+  $('#targetPicker .card-type')
+    .removeClass()
+    .addClass($('#cardModal .card-type').get(0).className);
+  $('#targetPicker .modal-body').text(
+    target == 'SELF' ? 'Who is playing this?' : 'Pick target.');
+  picker.show();
+
+  //"player" from js/ds.js
+  var playerSelector = target == 'SELF' ? '[data-player="' + player +'"]' : '';
+  var playerDiv = $('#state .player' + playerSelector);
+  var scrollTo =
+    playerDiv.offset().top
+    - picker.get(0).getBoundingClientRect().bottom;
+  window.scrollTo(0, scrollTo);
+
+  $('#cardModal').modal('hide');
+}
+function pickTarget(event) {
+  var picker = $('#targetPicker');
+  if (picker.css('display') == 'none') {
+    return;
+  }
+  var targetAnchor = $(event.target).closest('a');
+  var player = targetAnchor.closest('[data-player]').data('player').split(' ', 1)[0];
+  var region = targetAnchor.closest('[data-region]').data('region').split('-', 1)[0];
+  var coords = targetAnchor.data('coordinates');
+  var modal = $('#cardModal');
+  modal.data('target', player + ' ' + region + ' ' + coords);
+  tippy.hideAll({ duration: 0 });
+
+  var modesSelected = $('#cardModal .card-modes button.active');
+  playCard({target: modesSelected.eq(0)});
+  closeTargetPicker();
+  return false;
+}
+function closeTargetPicker() {
+  $('#targetPicker').hide();
+}
+function playCardCommand(disciplines, target) {
+  var modal = $('#cardModal');
+  var handIndex = modal.data('hand-coord');
+  var doNotReplace = modal.data('do-not-replace');
+  return 'play ' + handIndex
+         + (disciplines ? ' @ ' + disciplines.join(',') : '')
+         + (target == 'READY_REGION' ? ' ready' : '')
+         //+ (target == 'SELF' ? ' re ' + $('#cardModal .card-minions button.active').data('region-index') : '')
+         + (target == 'SELF' || target == 'SOMETHING' ? ' ' + modal.data('target') : '')
+         + (doNotReplace ? '' : ' draw');
+}
+function playCard(event) {
+  var button = $(event.target).closest('button'); //target might be inner p
+  var disciplines = [];
+  var target = null;
+  if (button.attr('id') == 'cardModalPlayButton') { //Multi-mode cards
+    $('#cardModal .card-modes button.active')
+      .each(function() { disciplines = disciplines.concat($(this).data('disciplines')); });
+    target = $('#cardModal .card-modes button.active:first').data('target');
+  }
+  else { //Single-mode cards
+    disciplines = button.data('disciplines');
+    target = button.data('target');
+  }
+
+  var command = playCardCommand(disciplines, target);
+  console.log(command);
+  DS.submitForm(
+      game, null, command, '', null, 'No',
+      $("#globalNotes").val(), $("#privateNotes").val(), {
+    callback: processData,
+    errorHandler: errorhandler
+  });
+  $('#cardModal').modal('hide');
+  return false;
+}
+function multiModeButtonClicked(event) {
+  var button = $(event.target).closest('button');
+  var delta = button.hasClass('active') ? -1 : 1;
+  var modes = $('#cardModal .card-modes button.active').length + delta;
+  var playButton = $('#cardModalPlayButton');
+  playButton.prop('disabled', modes < 1);
+  playButton.text(modes < 1 ? 'Select one or more disciplines' : 'Play');
+}
